c04924053d5d8a4bba42b28dd81b077ffb6ce3a4
paisakamao <45649228+paisakamao@users.noreply.github.com>
2025-08-13 18:53:25 +0500

    Create AppInfo.kt

---
+++ app/src/main/kotlin/com/deviceinfo/deviceinfoapp/model/AppInfo.kt
@@ -0,0 +1,9 @@
+package com.deviceinfo.deviceinfoapp.model
+
+import android.graphics.drawable.Drawable
+
+data class AppInfo(
+    val appName: String,
+    val packageName: String,
+    val icon: Drawable
+)
