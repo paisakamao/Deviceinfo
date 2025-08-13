327e9df4f800c88e764eb418908ebb518ed2fa17
paisakamao <45649228+paisakamao@users.noreply.github.com>
2025-08-13 22:46:48 +0500

    Update AppInfo.kt

--- app/src/main/kotlin/com/deviceinfo/deviceinfoapp/model/AppInfo.kt
+++ app/src/main/kotlin/com/deviceinfo/deviceinfoapp/model/AppInfo.kt
@@ -2,8 +2,14 @@ package com.deviceinfo.deviceinfoapp.model
 
 import android.graphics.drawable.Drawable
 
+/**
+ * An updated data class to hold detailed info for a single installed application.
+ */
 data class AppInfo(
     val appName: String,
     val packageName: String,
-    val icon: Drawable
+    val icon: Drawable,
+    val versionName: String,
+    val appSize: String,
+    val installerSource: String
 )
