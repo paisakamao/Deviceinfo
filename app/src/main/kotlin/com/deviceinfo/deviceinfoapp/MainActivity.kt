b0a45353c573202d01c708805587acb8483834f2
paisakamao <45649228+paisakamao@users.noreply.github.com>
2025-08-13 16:41:53 +0500

    Update MainActivity.kt

--- app/src/main/kotlin/com/deviceinfo/deviceinfoapp/MainActivity.kt
+++ app/src/main/kotlin/com/deviceinfo/deviceinfoapp/MainActivity.kt
@@ -14,24 +14,36 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Assuming your activity_main.xml has a RecyclerView with the id 'recyclerView'
         val recyclerView: RecyclerView = findViewById(R.id.recyclerView)
         recyclerView.layoutManager = LinearLayoutManager(this)
 
         val deviceInfoHelper = DeviceInfoHelper(this)
         val deviceInfoList = mutableListOf<DeviceInfo>()
 
-        // Get Device Information
+        // --- RAM Section ---
+        deviceInfoList.add(DeviceInfo("Total RAM", deviceInfoHelper.getTotalRam()))
+        deviceInfoList.add(DeviceInfo("Used RAM", deviceInfoHelper.getUsedRam()))
+        deviceInfoList.add(DeviceInfo("Free RAM", deviceInfoHelper.getAvailableRam()))
+
+        // --- Device Section ---
         deviceInfoList.add(DeviceInfo("Device Model", deviceInfoHelper.getDeviceModel()))
         deviceInfoList.add(DeviceInfo("Manufacturer", deviceInfoHelper.getManufacturer()))
+
+        // --- OS Section ---
         deviceInfoList.add(DeviceInfo("Android Version", deviceInfoHelper.getAndroidVersion()))
         deviceInfoList.add(DeviceInfo("SDK Version", deviceInfoHelper.getSDKVersion()))
-        deviceInfoList.add(DeviceInfo("RAM", deviceInfoHelper.getTotalRam()))
-        deviceInfoList.add(DeviceInfo("CPU", deviceInfoHelper.getCpuInfo()))
+        
+        // --- CPU Section ---
+        deviceInfoList.add(DeviceInfo("CPU Info", deviceInfoHelper.getCpuInfo()))
+
+        // --- Display Section ---
         deviceInfoList.add(DeviceInfo("Screen Resolution", deviceInfoHelper.getScreenResolution()))
         deviceInfoList.add(DeviceInfo("Screen Density", deviceInfoHelper.getScreenDensity()))
+
+        // --- Storage Section ---
         deviceInfoList.add(DeviceInfo("Total Internal Storage", deviceInfoHelper.getTotalInternalStorage()))
         deviceInfoList.add(DeviceInfo("Available Internal Storage", deviceInfoHelper.getAvailableInternalStorage()))
+
 
         val adapter = DeviceInfoAdapter(deviceInfoList)
         recyclerView.adapter = adapter
