cf9cdadf56e11b990eda0b81712ab9ae9c2ebac3
paisakamao <45649228+paisakamao@users.noreply.github.com>
2025-08-13 17:50:28 +0500

    Create SensorInfo.kt

---
+++ app/src/main/kotlin/com/deviceinfo/deviceinfoapp/model/SensorInfo.kt
@@ -0,0 +1,10 @@
+package com.deviceinfo.deviceinfoapp.model
+
+/**
+ * A data class to hold the detailed information for a single sensor.
+ */
+data class SensorInfo(
+    val name: String,
+    val vendor: String,
+    val type: String
+)
