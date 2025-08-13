0e6e3ebb4ea4da756bf5c49edcd155bd7287fdba
paisakamao <45649228+paisakamao@users.noreply.github.com>
2025-08-13 14:12:31 +0500

    Create DeviceInfo.kt

---
+++ app/src/main/kotlin/com/deviceinfo/deviceinfoapp/model/DeviceInfo.kt
@@ -0,0 +1,11 @@
+package com.deviceinfo.deviceinfoapp.model
+
+/**
+ * A simple data class to hold a piece of device information.
+ * @param label The name of the information (e.g., "Device Model").
+ * @param value The value of the information (e.g., "Pixel 6 Pro").
+ */
+data class DeviceInfo(
+    val label: String,
+    val value: String
+)```
