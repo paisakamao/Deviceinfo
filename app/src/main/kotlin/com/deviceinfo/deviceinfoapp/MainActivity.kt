4f1b8af4954e5cfbfc88895cf187eb3e6469c81f
paisakamao <45649228+paisakamao@users.noreply.github.com>
2025-08-15 21:30:47 +0500

    Update MainActivity.kt

--- app/src/main/kotlin/com/deviceinfo/deviceinfoapp/MainActivity.kt
+++ app/src/main/kotlin/com/deviceinfo/deviceinfoapp/MainActivity.kt
@@ -20,7 +20,7 @@ class MainActivity : AppCompatActivity() {
         val recyclerView: RecyclerView = findViewById(R.id.recyclerView)
         recyclerView.layoutManager = LinearLayoutManager(this)
 
-        // Create instances of all non-battery helpers
+        // All helpers are correctly instantiated
         val deviceInfoHelper = DeviceInfoHelper(this)
         val cpuInfoHelper = CpuInfoHelper()
         val displayInfoHelper = DisplayInfoHelper(this)
@@ -31,18 +31,24 @@ class MainActivity : AppCompatActivity() {
         val deviceInfoList = mutableListOf<DeviceInfo>()
 
         // --- Populate the full dashboard list ---
-        deviceinfoList.add(DeviceInfo("Total RAM", deviceInfoHelper.getTotalRam()))
-        deviceinfoList.add(DeviceInfo("Internal Storage Used", deviceInfoHelper.getInternalStorageUsagePercentage()))
-        deviceinfoList.add(DeviceInfo("CPU Model", cpuInfoHelper.getCpuModel()))
-        deviceinfoList.add(DeviceInfo("Root Status", systemInfoHelper.getRootStatus()))
-        deviceinfoList.add(DeviceInfo("Kernel Version", systemInfoHelper.getKernelVersion()))
-        
+        deviceInfoList.add(DeviceInfo("Total RAM", deviceInfoHelper.getTotalRam()))
+        deviceInfoList.add(DeviceInfo("Used RAM", deviceInfoHelper.getUsedRam()))
+        deviceInfoList.add(DeviceInfo("Internal Storage Used", deviceInfoHelper.getInternalStorageUsagePercentage()))
+        deviceInfoList.add(DeviceInfo("CPU Model", cpuInfoHelper.getCpuModel()))
+        deviceInfoList.add(DeviceInfo("Number of Cores", cpuInfoHelper.getNumberOfCores()))
+        deviceInfoList.add(DeviceInfo("Screen Resolution", displayInfoHelper.getScreenResolution()))
+        deviceInfoList.add(DeviceInfo("Refresh Rate", displayInfoHelper.getRefreshRate()))
+        deviceInfoList.add(DeviceInfo("Root Status", systemInfoHelper.getRootStatus()))
+        deviceInfoList.add(DeviceInfo("Kernel Version", systemInfoHelper.getKernelVersion()))
+        deviceInfoList.add(DeviceInfo("Device Model", deviceInfoHelper.getDeviceModel()))
+        deviceInfoList.add(DeviceInfo("Android Version", deviceInfoHelper.getAndroidVersion()))
+
         // --- Clickable Summary Items ---
-        deviceinfoList.add(DeviceInfo("Battery Details", getBatteryPercentageForDashboard()))
-        deviceinfoList.add(DeviceInfo("Sensor Details", sensorInfoHelper.getSensorDetailsList().size.toString() + " Sensors"))
-        deviceinfoList.add(DeviceInfo("Application Details", appInfoHelper.getAllAppsDetails().size.toString() + " Apps"))
+        deviceInfoList.add(DeviceInfo("Battery Details", getBatteryPercentageForDashboard()))
+        deviceInfoList.add(DeviceInfo("Sensor Details", sensorInfoHelper.getSensorDetailsList().size.toString() + " Sensors"))
+        deviceInfoList.add(DeviceInfo("Application Details", appInfoHelper.getAllAppsDetails().size.toString() + " Apps"))
         
-        val adapter = DeviceInfoAdapter(deviceinfoList)
+        val adapter = DeviceInfoAdapter(deviceInfoList)
         
         adapter.onItemClick = { deviceInfo ->
             when (deviceInfo.label) {
@@ -55,7 +61,6 @@ class MainActivity : AppCompatActivity() {
         recyclerView.adapter = adapter
     }
 
-    // Self-contained helper function for the dashboard battery level
     private fun getBatteryPercentageForDashboard(): String {
         val intent = registerReceiver(null, IntentFilter(Intent.ACTION_BATTERY_CHANGED))
         val level = intent?.getIntExtra(BatteryManager.EXTRA_LEVEL, -1) ?: -1
