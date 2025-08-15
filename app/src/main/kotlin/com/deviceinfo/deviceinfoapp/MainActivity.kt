08fe2f3b3b75331cf6bb007c5a46f0f1f4d66f16
paisakamao <45649228+paisakamao@users.noreply.github.com>
2025-08-15 11:03:21 +0500

    Update MainActivity.kt

--- app/src/main/kotlin/com/deviceinfo/deviceinfoapp/MainActivity.kt
+++ app/src/main/kotlin/com/deviceinfo/deviceinfoapp/MainActivity.kt
@@ -18,46 +18,23 @@ class MainActivity : AppCompatActivity() {
         val recyclerView: RecyclerView = findViewById(R.id.recyclerView)
         recyclerView.layoutManager = LinearLayoutManager(this)
 
-        // Create instances of all our helpers
         val deviceInfoHelper = DeviceInfoHelper(this)
         val batteryInfoHelper = BatteryInfoHelper(this)
         val cpuInfoHelper = CpuInfoHelper()
         val displayInfoHelper = DisplayInfoHelper(this)
         val sensorInfoHelper = SensorInfoHelper(this)
         val appInfoHelper = AppInfoHelper(this)
-        val systemInfoHelper = SystemInfoHelper() // New helper instance
+        val systemInfoHelper = SystemInfoHelper()
         
         val deviceInfoList = mutableListOf<DeviceInfo>()
 
-        // --- RAM Section ---
+        // Populate the full summary list
         deviceInfoList.add(DeviceInfo("Total RAM", deviceInfoHelper.getTotalRam()))
-        deviceInfoList.add(DeviceInfo("Used RAM", deviceInfoHelper.getUsedRam()))
-        
-        // --- Storage Section ---
         deviceInfoList.add(DeviceInfo("Internal Storage Used", deviceInfoHelper.getInternalStorageUsagePercentage()))
-        
-        // --- Battery Section ---
         deviceInfoList.add(DeviceInfo("Battery Level", batteryInfoHelper.getBatteryPercentage()))
-        deviceInfoList.add(DeviceInfo("Battery Temperature", batteryInfoHelper.getBatteryTemperature()))
-        
-        // --- CPU Section ---
         deviceInfoList.add(DeviceInfo("CPU Model", cpuInfoHelper.getCpuModel()))
-        
-        // --- Display Section ---
         deviceInfoList.add(DeviceInfo("Screen Resolution", displayInfoHelper.getScreenResolution()))
-        deviceInfoList.add(DeviceInfo("Refresh Rate", displayInfoHelper.getRefreshRate()))
-
-        // --- System Section ---
         deviceInfoList.add(DeviceInfo("Root Status", systemInfoHelper.getRootStatus()))
-        deviceInfoList.add(DeviceInfo("Kernel Version", systemInfoHelper.getKernelVersion()))
-        deviceInfoList.add(DeviceInfo("Build ID", systemInfoHelper.getBuildId()))
-        deviceInfoList.add(DeviceInfo("Java VM Version", systemInfoHelper.getJavaVmVersion()))
-
-        // --- Device & OS Section ---
-        deviceInfoList.add(DeviceInfo("Device Model", deviceInfoHelper.getDeviceModel()))
-        deviceInfoList.add(DeviceInfo("Android Version", deviceInfoHelper.getAndroidVersion()))
-
-        // --- Clickable Summary Items ---
         deviceInfoList.add(DeviceInfo("Total Sensors", sensorInfoHelper.getSensorDetailsList().size.toString()))
         deviceInfoList.add(DeviceInfo("All Apps", appInfoHelper.getAllAppsDetails().size.toString()))
         
@@ -66,12 +43,13 @@ class MainActivity : AppCompatActivity() {
         adapter.onItemClick = { deviceInfo ->
             when (deviceInfo.label) {
                 "Total Sensors" -> {
-                    val intent = Intent(this, SensorListActivity::class.java)
-                    startActivity(intent)
+                    startActivity(Intent(this, SensorListActivity::class.java))
                 }
                 "All Apps" -> {
-                    val intent = Intent(this, AppListActivity::class.java)
-                    startActivity(intent)
+                    startActivity(Intent(this, AppListActivity::class.java))
+                }
+                "Battery Level" -> {
+                    startActivity(Intent(this, BatteryDetailActivity::class.java))
                 }
             }
         }
